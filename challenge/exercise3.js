37ecc30d1ba80d4514a89c0ffbe79c6bbe98ecf9
GabrielAB2411 <gabrielab2411@gmail.com>
2021-02-28 01:51:58 -0300

    finished

---
+++ challenge/exercise3.js
@@ -0,0 +1,49 @@
+const txt = "zzhtcsch sqz jqvtvl vns jlrpqq prjg wrghkrmv hdz mxx qvmh ngg kmplb vrvzpn sfvrqm hdkm nqqrwlmq bmxxln qgfpt wgmn fcs dgzzz kgxsmv qtbr gmmvhnfj jvvzhl xbdbhgn jzms jtqc lpxxtxd smm xhjwdjn fgczc hxxwp jqr vpztrvcl bjgmkccr ktxlwnr vjzpcl xbg shc wjzqmww kpbkbjzj psppq gbncrdn prlmmjl tfrnn qmcjhk lhlgnm dmvnt mljg xdg vvjx grmhcn thvvpw tfxjxl pwwcbm qkfbrfks bkhnffcz tttgq zrvbg dxjvvkc rcjh cvp qgzgn jrjnvv pfh pcksqscx lbm wpmfpj jpm bchnjbss dgqpcnlf tspxv ffczq dfp knvq dhfvxzv jhhxzr mjx fsb gtd cvklph bdsgmtr mclbllrx dlkdbpcc rjpvrvxt hsbcxtr qkrgtmw wwcrmckx hrplwzjb vzcqxp nlrbbfnz pwtmmtbb vfpwt khxgrbbs dcscpkt dldx bfdz xvq gfvzx mlp qqhzv nzvdgfdz sqbvtz hdr ptpvt rgrgjr vsng gcsdqnvd jbhfv zzq mbp plwlv nvxhn jzwtdnjr hmlbvs ngqdn blr hkgjqk dbt wrnzhz bjjkwf wdxh qnqgjcmk qhkqf mhszkzn tfxkc bfvgkd mfjkbchx lqc nnvvmr jhcr vtqvvmhl vmxzqgw brpg tcdhdh ktgpcnl rkfxmbb rzdlqr pbrxwg gmsr pjl vtxtbw kjvw mnlcfm wlpbnvmh nxfhdd gkp rsjbksz rjxwbjck zhtxhnw kkkqltcj dqnz mdsqbjn ptm lhj ggxzj dmmzsd jchdrrx sxbjkl zlxqkpjh fpxmn cfczwdq gczkchl djfxw vqkrmb qzcxr vmswpj cqssd kdzqhw xxq ncvhppg snfc hnjgbtcz klnrqp zbffh nbpznvl sknppkq xqd fdtqld qbd cbpc mdhlxjlc wngcvvs wpbcggw nfckbph wcz gpcqwfnm jvbwcvx fkc drll xwh mxgsz xgrvk cfnbvgc whqt cpbdt fftrzzj bmpg gfsxvt sqtpzh rlwzsp jgj lnxxwts zvdjvzwf pzk nsmw nxm pcnbsgk pdr wphlgkk kdv fwbwx nrknnw ckcrkn qfkqbxs wblpgmt ndc pfwgmt wstz rhw pmmk dhdpqs jwtps xcsjdmqd mpzmh tftsntvl cbtx rfx rfkflbrl msxl khxpm jfjqtthw gxffj dfskpcqg dplrnkb vdbh khjn pkhcqv jzxxmcr nkp mpvl rgmfrbz vvpsnr cdptv bxdpzcmb rdzfzssf zxzvgkl gbpq fgxqxkxv vmgbgs kdjc xhnv lknx gvjpksm wpkjbtc ltprfkmr plwgkd qzctb ftx vwcj dpxrdvsb dbvwzx vwglzdqx wsdgdwm zghwvz pkwsgpkq cvl xqfhxxqm pltnmkxp gvsgfk hfmbpn cphwjsf bkkjbv dvvlf wprn sqbs jdbpfg qcmbtpn rcnqb plpv ppmbk lwqtczct mksrstj kqhzd qfvq wfcx lrtsmb ftvcl kwrgbqgq mhcxdksh rjtm jlvmqk bmvl tvj qnx sxl dzbcrjdt zgqrrthv mtqlvbz nsvps plk bgtck qxqscnn dhvrbd hmphlgk tzprqrzd qkjf fsdjq cljqc bcqpt jbsfpjvf jdb dgjgscq hghvwn jxcx lfsck cpqdbzq hmm cwrl rcsrxbg zsfcgl hftwpzwm qxflc vzkmmb jpqhj grkklsd zvhzh zdtnv sjlbx khblvds bnmblz lsjfjgk xhtwncs bjh kqcmtsfc wlpdztrq ptw vgw gjsm vdphfj dqt gljrhbjm hpvnp rrfl fbdcsp hxppvvdx bvfg qhkgsjb vrtkkk cwbrg gkswdfk qbhzgqll lmfbhf hbbfw gflx hwmt mlvhjc xpzmm fkrwxt zxp slnkxhh tfnczfv lpw pnhtlz jslzn gnrxr fjngzp qmw bsc hlnl jbbgxh vlvwzskt wqp fxjp btpmdgjw mndzhhrn dmrh qlqwfqw qdf kbbbdc qhw bdszqpld jscwzcs kgmcbxbw vrqdrnnk wrvkqchh btnz fqcgqrpd kvcn pqq fkxxnlq fjs mhjpksdm lhhp jpgpxmpp mnrwkxmf mtlv zjz kmwtfn bqgv dbnstvs gtjdbsxd xxxdqtqs xckd rrmtrhvl qznzjwps lsplljd vrmlt wtzjb dqqdj rxtg tdm jfllw tgxwxq fhrxczdt jrhw dxhhbfx lpp ljmzdcc vjm xzglplfm sdswmzhb gqbwc sxbmm gblmp nvjfvgr csxcj rrblscgh wvg dnmndfdb qxfsnpfd vzkftwnn rzfmgmkg qhfgjqc hwnklbn gdvcghsj fgjbrl gsdvgmb mkkqzv zlrtnv vfb qbvv bqqz dtrct mnkgwksn hcc zvvl xqp gdmzq cbpqd lfjxrt jzxzgnqg zrnwswv djmb bbmzjrk pfjddmh vljpq zjfvlm mcgwdb cld rczmp gcpqszhd vzk hhdmkrm xwzdtc rfjzcbr wsmfd phk dbln vtjrn rmzq njrk rqtkkvmz gmgzbq zwj hkc cnxkt lwkjg vqbl bxhcq svn sqlqcts rtjm flpdgpdn nggj bhx xcxrv cdrnj sxrnffz mcxmv zdq jsmhc fxwdvkcf jddq jqpf prxwnc whb gtqf jfnfb pwhvzvb lgxz mgp fczxw lctrrb xwmvsrwt pcddvpvq rrk vgdp fjzgl sgtd vsztcts dbdw kcwwg jsxswbs zcmsml qnsz ghfsg ccjrs zbdpxwb qwpx sqstkmfc mhpfb btdvdfll jdrp xxgjkkv fzng ntz clwktq kjvqtclf phdhf fvd jncp khrpjz tlkk jsgzbtp kdgnh qdbtmf wkltvsj ptjs vbvhqmp fqgqqwn kwp xvvk vtsrjk fmf zwlfrrf hrwltw cktptxt nnbgxdv rkmf zjkhh zqw qtwpggd kwl tlxzml chwxqqz bmwv tknbv ccrrzss wksdl vpdm sxl twd ghj cldzlpbj tjxphrbl ntqtr bzqnrlhk gzksgn pqmq vdg pzft fqxqlfp wxwc tffjh crlf mblq bhjprg gwkfkk dqs hvsfhns qjkf dkzwxwb dwr xvw zwsptc nzpnflm vbdndvc hwsrt hvsqth qjgrd klzs vwrjh hrznwg xzpf fmp rxkjpz pmnjknt bndnfntl";
+const words = txt.split(" ");
+const googlonAlphabet = "bcdfghjklmnpqrstvwxz";
+const normalAlphabet =   "abcdefghijklmnopqrst";
+let normalWords = new Set();
+let prettyNumbers = 0;
+
+//Transforma as palavras do texto em numeros conforme parâmetros dados no enunciado 
+const toNumber = word => {
+    let numbers = 0;
+    for (let i = 0; i < word.length; i++) {
+      const wordsArray = word[i];
+      const value = googlonAlphabet.indexOf(wordsArray); 
+      numbers += value * Math.pow(20, i);
+    }
+    return numbers;
+  };
+
+//Transforma as palavras em googlon para alfabeto convencional
+    const toNormal = word => {
+    let string = "";
+    for (let i = 0; i < word.length; i++) {
+      const wordsArray = word[i];
+      const pos = googlonAlphabet.indexOf(wordsArray);
+      string += normalAlphabet[pos];
+    }
+    return string;
+  };
+
+//Faz a contagem de números bonitos
+  words.forEach(word => {
+   
+    const normalWord = toNormal(word);
+
+    if (!normalWords.has(normalWord)) {
+      normalWords.add(normalWord);
+
+      let number = toNumber(word);
+
+      if (number >= 563131 && number % 5 === 0) {
+        prettyNumbers++;
+      }
+    }
+  });
+
+//Retorna os resultados
+  console.log("NÚMEROS BONITOS:", prettyNumbers)
+  
+  
